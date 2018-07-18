d9b729c171bc7db095962b9e8ac65414580ec5ba
Petri Behm <petri.behm@paf.com>
2018-07-18 16:07:28 +0300

    Part 1 - code

---
+++ src/components/BookList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ data().title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BookList',
+    methods: {
+      data: function() {
+        return {
+          title: "All Books"
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+</style>

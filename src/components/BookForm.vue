9f9647521eb14a470e0d86ccb857900482c88b81
Petri Behm <petri.behm@paf.com>
2018-07-18 17:14:57 +0300

    Creating a BookForm Component

---
+++ src/components/BookForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form class="" action="#" method="post">
+    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <button @submit.prevent="bookSubmit(bookTitle, bookAuthor)" type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'BookForm',
+  props: [ 'books' ],
+  data: function() {
+    return {
+      bookTitle: '',
+      bookAuthor: ''
+    }
+  },
+  methods: {
+    bookSubmit: function(bookTitle, bookAuthor) {
+      this.$emit('addBook', bookTitle, bookAuthor);
+    }
+  }
+}
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,37 +4,45 @@
     <ul>
       <book-item v-for='book in books' :book='book'></book-item>
     </ul>
+    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
-  import BookItem from './BookItem';
+import BookItem from './BookItem';
+import BookForm from './BookForm';
 
-  export default {
-    name: 'BookList',
-    data: function() {
-        return {
-          books: [
-            { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
-            { title: 'American Gods', author: 'Neil Gaiman' },
-            { title: 'Amusing Ourselves to Death', author: 'Neil Postman' }
-          ]
-        }
-    },
-    components: {
-      BookItem
+export default {
+  name: 'BookList',
+  data: function() {
+    return {
+      books: [
+        { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+        { title: 'American Gods', author: 'Neil Gaiman' },
+        { title: 'Amusing Ourselves to Death', author: 'Neil Postman' }
+      ]
+    }
+  },
+  components: {
+    BookItem,
+    BookForm
+  },
+  methods: {
+    appendBook: function(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor })
     }
   }
+}
 </script>
 
 <style>
-  h1,
-  h2 {
-    font-weight: normal;
-  }
+h1,
+h2 {
+  font-weight: normal;
+}
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 </style>

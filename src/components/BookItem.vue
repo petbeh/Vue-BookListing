efee08ae2bf24e28cae3238edde0c5f4ffffb348
Petri Behm <petri.behm@paf.com>
2018-07-18 16:49:21 +0300

    Creating a child component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li> {{book.title}}: {{book.author}} </li>
+</template>
+
+<script>
+export default {
+  name: 'BookItem',
+  data: function() {
+      return {
+        props: 'book'
+      }
+  }
+}
+</script>
+
+<style>
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+</style>
